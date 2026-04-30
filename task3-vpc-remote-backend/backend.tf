0f77f29e60e6e728b150064a8ce39c30c1e8d848
Doni Sehrawat <donisehrawat01@gmail.com>
2026-04-30 07:32:32 +0530

    Delete task3-vpc-remote-backend directory

--- task3-vpc-remote-backend/backend.tf
+++
@@ -1,8 +0,0 @@
-terraform {
-  backend "s3" {
-    bucket         = "my-terraform-state-bucket-12345"
-    key            = "vpc/terraform.tfstate"
-    region         = "ap-south-1"
-    dynamodb_table = "terraform-lock-table"
-  }
-}
